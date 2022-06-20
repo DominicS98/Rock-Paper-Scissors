6638574bc2f2ee36a97800067b0d7b04e33a41ce
Dominic Sumandan <93337211+DominicS98@users.noreply.github.com>
2022-06-20 11:37:30 +0300

    Add files via upload

---
+++ Rock, Paper, Scissors/app.js
@@ -0,0 +1,46 @@
+//A simple rock, paper, scissors game to practice switch and if statements. A random number generates
+// a florred number that functions as a RNG game. Simple and clean.
+let playerWeapon = (prompt("Let's play Rock, Paper, Scissors. Type your choise").toLocaleLowerCase());
+let computerWeapon = Math.floor((Math.random()*3 + 1));
+ //Random number gets a 3 random possibility for an output 
+switch(computerWeapon) {
+    case 1:
+        console.log("Your adversary chose Rock");
+        break;
+    case 2:
+        console.log("The PC got some Paper");
+        break;
+    case 3:
+        console.log("The AI is running towards you with some Scissors!")
+        break;
+        default:
+            console.log(`For some reason, the output is ${computerWeapon}`)
+ }
+ //3*3 possible outcomes
+if ((computerWeapon === 1) && (playerWeapon === "rock")) {
+    console.log("You both have rock, sparks but no fire, it's a draw!");
+ } 
+if ((computerWeapon === 1) && (playerWeapon === "paper")) {
+    console.log("You wrap the rock with your paper, you win!");
+ } 
+if ((computerWeapon === 1) && (playerWeapon === "scissors")) {
+    console.log("The rock smashes and breaks you puny scissor, you lose!");
+ } 
+if ((computerWeapon === 2) && (playerWeapon === "rock")) {
+    console.log("The paper wraps your rock, you lose!");
+ } 
+if ((computerWeapon === 2) && (playerWeapon === "paper")) {
+    console.log("You both have papers and make some origami with it, draw!");
+ } 
+if ((computerWeapon === 2) && (playerWeapon === "scissors")) {
+    console.log("You cut the paper in a billion pieces, you win!");
+ } 
+if ((computerWeapon === 3) && (playerWeapon === "rock")) {
+    console.log("You smash the scissor intro a metal pulp, you win!");
+ } 
+if ((computerWeapon === 3) && (playerWeapon === "paper")) {
+    console.log("Your paper is cut and turned intro dust, you lose!");
+ } 
+if ((computerWeapon === 3) && (playerWeapon === "scissors")) {
+    console.log("You got both scissors, draw!");
+ } 
